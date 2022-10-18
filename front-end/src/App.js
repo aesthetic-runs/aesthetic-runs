96c8f494a69feb1b803a0f05cd0c13513064d00a
Matthew Apuya <m.apuya19@gmail.com>
2022-10-18 16:42:09 -0400

    refactor w/ backend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          Hello World
         </p>
         <a
           className="App-link"
