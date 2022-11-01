47dd7aef479fd10ae8f5af0dcaba8ecfa0b85916
Matthew Apuya <m.apuya19@gmail.com>
2022-11-01 14:16:30 -0400

    add registration endpoint w/ bcrypt

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 // require database connection
 const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
 
 // execute database connection
 dbConnect();
@@ -17,5 +18,45 @@ app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
 });
+
+app.post("/registration", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+bcrypt.hash(request.body.password, 10).then().catch();
 
 module.exports = app;
