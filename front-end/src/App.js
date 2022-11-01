d954c594e5c47fa54fa74c01286421f8000a22c1
Matthew Apuya <m.apuya19@gmail.com>
2022-10-31 21:29:35 -0400

    remove boilerplate extras for demo 1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Hello World
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
