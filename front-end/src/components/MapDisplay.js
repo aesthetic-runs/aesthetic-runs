f60ee175bca22b3971fcb02acf397a2f4fcb11da
carolineytian <carolineytian@gmail.com>
2022-11-08 18:24:56 -0500

    Adding start/end locations to Google Map componenet

--- front-end/src/components/MapDisplay.js
+++ front-end/src/components/MapDisplay.js
@@ -23,6 +23,34 @@ const MapDisplay = () => {
 
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS;
 
+  function checkWalking({ target: { checked } }) {
+    checked &&
+      this.setState(() => ({
+        travelMode: "WALKING",
+      }));
+  }
+
+  function getOrigin(ref) {
+    this.origin = ref;
+  }
+
+  function getDestination(ref) {
+    this.destination = ref;
+  }
+
+  function onClick() {
+    if (this.origin.value !== "" && this.destination.value !== "") {
+      this.setState(() => ({
+        origin: this.origin.value,
+        destination: this.destination.value,
+      }));
+    }
+  }
+
+  function onMapClick(...args) {
+    console.log("onClick args: ", args);
+  }
+
   return (
     <LoadScript googleMapsApiKey={apiKey}>
       <GoogleMap
@@ -31,6 +59,55 @@ const MapDisplay = () => {
         center={currentPosition}
       >
         {currentPosition.lat && <Marker position={currentPosition} />}
+
+     {/* This is a WIP */}
+        {/* <div className="map">
+          <div className="map-settings">
+            <hr className="mt-0 mb-3" />
+
+            <div className="row">
+              <div className="col-md-6 col-lg-4">
+                <div className="form-group">
+                  <label htmlFor="ORIGIN">Origin</label>
+                  <br />
+                  <input
+                    id="ORIGIN"
+                    className="form-control"
+                    type="text"
+                    ref={this.getOrigin}
+                  />
+                </div>
+              </div>
+
+              <div className="col-md-6 col-lg-4">
+                <div className="form-group">
+                  <label htmlFor="DESTINATION">Destination</label>
+                  <br />
+                  <input
+                    id="DESTINATION"
+                    className="form-control"
+                    type="text"
+                    ref={this.getDestination}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="form-group custom-control custom-radio mr-4">
+              <input
+                id="WALKING"
+                className="custom-control-input"
+                name="travelMode"
+                type="radio"
+                checked={this.state.travelMode === "WALKING"}
+                onChange={this.checkWalking}
+              />
+              <label className="custom-control-label" htmlFor="WALKING">
+                Walking
+              </label>
+            </div>
+          </div>
+        </div> */}
       </GoogleMap>
     </LoadScript>
   );
