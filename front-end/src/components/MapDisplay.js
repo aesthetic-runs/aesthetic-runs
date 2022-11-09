7452f0e993190b26ed707324dbf8860021c30607
carolineytian <carolineytian@gmail.com>
2022-11-08 19:05:10 -0500

    Update MapDisplay.js

--- front-end/src/components/MapDisplay.js
+++ front-end/src/components/MapDisplay.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import TextField from "@mui/material/TextField";
 
 const MapDisplay = () => {
   const mapStyles = {
@@ -53,7 +54,37 @@ const MapDisplay = () => {
   }
 
   return (
-    <LoadScript googleMapsApiKey={apiKey}>
+    <div>
+        {/* <div className="row">
+              <div className="col-md-6 col-lg-4">
+                <div className="form-group">
+                  <label htmlFor="ORIGIN">Origin</label>
+                  <TextField htmlFor="ORIGIN" label="ORIGIN"/>
+                  <br />
+                  <input
+                    id="ORIGIN"
+                    className="form-control"
+                    type="text"
+                    ref={this.getOrigin}
+                  />
+                </div>
+              </div>
+
+              <div className="col-md-6 col-lg-4">
+                <div className="form-group">
+                  <label htmlFor="DESTINATION">Destination</label>
+                  <br />
+                  <input
+                    id="DESTINATION"
+                    className="form-control"
+                    type="text"
+                    ref={this.getDestination}
+                  />
+                </div>
+              </div>
+            </div> */}
+
+      <LoadScript googleMapsApiKey={apiKey}>
       <GoogleMap
         mapContainerStyle={mapStyles}
         zoom={13}
@@ -66,21 +97,21 @@ const MapDisplay = () => {
           <div className="map-settings">
             <hr className="mt-0 mb-3" />
 
-            <div className="row">
+            {/* <div className="row">
               <div className="col-md-6 col-lg-4">
                 <div className="form-group">
                   <label htmlFor="ORIGIN">Origin</label>
                   <br />
-                  {/* <input
+                  <input
                     id="ORIGIN"
                     className="form-control"
                     type="text"
                     ref={this.getOrigin}
-                  /> */}
+                  />
                 </div>
               </div>
 
-              {/* <div className="col-md-6 col-lg-4">
+              <div className="col-md-6 col-lg-4">
                 <div className="form-group">
                   <label htmlFor="DESTINATION">Destination</label>
                   <br />
@@ -91,8 +122,8 @@ const MapDisplay = () => {
                     ref={this.getDestination}
                   />
                 </div>
-              </div> */}
-            </div>
+              </div>
+            </div> */}
 
             {/* <div className="form-group custom-control custom-radio mr-4">
               <input
@@ -111,6 +142,9 @@ const MapDisplay = () => {
         </div>
       </GoogleMap>
     </LoadScript>
+    </div>
+  
+    
   );
 };
 
