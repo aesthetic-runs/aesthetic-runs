f78fffba1074201aaf9696e741197f060d097a9b
carolineytian <carolineytian@gmail.com>
2022-11-29 10:41:19 -0800

    Clean Profile.js code warnings

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,17 +1,4 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { Link as RouterLink } from "react-router-dom";
-import Link from "@mui/material/Link";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import {
   MDBCol,
@@ -22,8 +9,8 @@ import {
   MDBCardBody,
   MDBCardImage,
   MDBBtn,
-  MDBBreadcrumb,
-  MDBBreadcrumbItem,
+  //MDBBreadcrumb,
+  //MDBBreadcrumbItem,
   MDBProgress,
   MDBProgressBar,
   MDBIcon,
