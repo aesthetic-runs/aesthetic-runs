e67d3d6736305f9ce9a8ad56a248ced4c46ae364
carolineytian <carolineytian@gmail.com>
2022-12-06 00:28:15 -0500

    Create Empire State Quick Run page

---
+++ front-end/src/pages/EmpireStateQuickRun.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+// import MapDisplay from "../components/MapDisplay";
+// import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+class GoogleMaps extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentLocation: { lat: 40.756795, lng: -73.954298 },
+      key: process.env.REACT_APP_GOOGLE_MAPS
+    };
+  }
+
+  render() {
+    const apiIsLoaded = (map, maps) => {
+      const directionsService = new window.google.maps.DirectionsService();
+      const directionsRenderer = new window.google.maps.DirectionsRenderer();
+      directionsRenderer.setMap(map);
+      //origin is set to eataly NYC flatiron location (200 5th Avenue)
+      const origin = { lat: 40.742213, lng: -73.989588 };
+
+      //destination is set to empire state building
+      // const destination = { lat: 41.756795, lng: -78.954298 };
+      const destination = { lat: 40.748817, lng: -73.985428 };
+
+      directionsService.route(
+        {
+          origin: origin,
+          destination: destination,
+          travelMode: window.google.maps.TravelMode.WALKING
+        },
+        (result, status) => {
+          if (status === window.google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+          } else {
+            console.error(`error fetching directions ${result}`);
+          }
+        }
+      );
+    };
+    return (
+      <div>
+        <div style={{ height: "600px", width: "100%" }}>
+        {/* <div className="Map">
+          <MapDisplay />
+        </div> */}
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              //key: "YOUR_API_KEY"
+              key: process.env.REACT_APP_GOOGLE_MAPS
+              // key: "AIzaSyDbkwPEnUrm0tS38IADZupex-I30a7txp4"
+            }}
+            defaultCenter={{ lat: -3.745, lng: -38.523 }}
+            defaultZoom={10}
+            center={this.state.currentLocation}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => apiIsLoaded(map, maps)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+export default GoogleMaps;
+
+//"use strict";
+
+/* <script src="https://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
+
+let DirectionsService = new window.google.maps.DirectionsService();
+
+function calcRoute() {
+
+    var waypts = [];
+
+    for (var i in window.owt.stores.spotStore.data.map) {
+        waypts.push({
+            location:  new window.google.maps.LatLng(12.3, -33.6),
+            stopover:true
+        });
+        console.log(waypts);
+    }
+    var request = {
+        origin: new window.google.maps.LatLng(50.82788, 3.26499),
+        destination: new window.google.maps.LatLng(50.82788, 3.26499),
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: window.google.maps.DirectionsTravelMode.DRIVING
+    };
+
+    window.directionsService.route(request, function(response, status) {
+        if (status == window.google.maps.DirectionsStatus.OK) {
+            window.directionsDisplay.setDirections(response);
+        }
+    });
+}
+; */
