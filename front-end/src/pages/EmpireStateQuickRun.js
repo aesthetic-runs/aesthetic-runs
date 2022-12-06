7f56dfd740d5dac8ee645a88960b629aac01e629
carolineytian <carolineytian@gmail.com>
2022-12-06 01:43:46 -0500

    Clean EmpireStateQuickRun.js code

--- front-end/src/pages/EmpireStateQuickRun.js
+++ front-end/src/pages/EmpireStateQuickRun.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+// import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 // import MapDisplay from "../components/MapDisplay";
 // import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
@@ -85,38 +85,3 @@ class GoogleMaps extends Component {
   }
 }
 export default GoogleMaps;
-
-
-//"use strict";
-
-/* <script src="https://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
-
-let DirectionsService = new window.google.maps.DirectionsService();
-
-function calcRoute() {
-
-    var waypts = [];
-
-    for (var i in window.owt.stores.spotStore.data.map) {
-        waypts.push({
-            location:  new window.google.maps.LatLng(12.3, -33.6),
-            stopover:true
-        });
-        console.log(waypts);
-    }
-    var request = {
-        origin: new window.google.maps.LatLng(50.82788, 3.26499),
-        destination: new window.google.maps.LatLng(50.82788, 3.26499),
-        waypoints: waypts,
-        optimizeWaypoints: true,
-        travelMode: window.google.maps.DirectionsTravelMode.DRIVING
-    };
-
-    window.directionsService.route(request, function(response, status) {
-        if (status == window.google.maps.DirectionsStatus.OK) {
-            window.directionsDisplay.setDirections(response);
-        }
-    });
-}
-; */
-
