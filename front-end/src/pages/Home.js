a22c270db36a2205e165a9702bf550a9a965882a
Matthew Apuya <m.apuya19@gmail.com>
2022-12-12 23:23:41 -0500

    more cleaning

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-// import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
