faa2bee6760d5184d6e323756ba36891e11799b8
KP <cp3111@nyu.edu>
2022-12-13 02:19:41 -0500

    hover descriptions added for all in prcentralpark

--- front-end/src/pages/PopularRunCentralPark.js
+++ front-end/src/pages/PopularRunCentralPark.js
@@ -19,14 +19,14 @@ const PopularRunCentralPark = () => {
     }
 
     const directionsService = new window.google.maps.DirectionsService();
-    const directionsRenderer = new window.google.maps.DirectionsRenderer();
+    const directionsRenderer = new window.google.maps.DirectionsRenderer(rendererOptions);
     directionsRenderer.setMap(map);
 
     const image1 = "https://www.fitnyc.edu/images/cer/master-brand-button.jpg";
     const beachMarker1 = new window.google.maps.Marker({
       position: { lat: 40.79939, lng: -73.952404 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "Central Park North (110 St): 110th Street is a street in the New York City borough of Manhattan. It is commonly known as the boundary between Harlem and Central Park, along which it is known as Central Park North.",
       icon: {
         size: new window.google.maps.Size(40, 30),
         scaledSize: new window.google.maps.Size(40, 30),
@@ -38,7 +38,7 @@ const PopularRunCentralPark = () => {
     const beachMarker2 = new window.google.maps.Marker({
       position: { lat: 40.79765, lng: -73.956022 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "North Woods Loch Waterfall: The Loch, known for its three waterfalls, is the long, narrow watercourse that flows through the Ravine in the North Woods.",
       icon: {
         size: new window.google.maps.Size(40, 30),
         scaledSize: new window.google.maps.Size(40, 30),
@@ -50,7 +50,7 @@ const PopularRunCentralPark = () => {
     const beachMarker3 = new window.google.maps.Marker({
       position: { lat: 40.79508, lng: -73.959318 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "Glen Span Arch: Glen Span Arch, carrying the West Drive across 102nd Street, serves as a gateway to the wooded and secluded Loch to the northeast.",
       icon: {
         size: new window.google.maps.Size(40, 30),
         scaledSize: new window.google.maps.Size(40, 30),
@@ -62,7 +62,7 @@ const PopularRunCentralPark = () => {
     const beachMarker4 = new window.google.maps.Marker({
       position: { lat: 40.787578, lng: -73.962631 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "Jacqueline Kennedy Onassis Reservoir: The Jacqueline Kennedy Onassis Reservoir, also known as Central Park Reservoir, is a decommissioned reservoir in Central Park in the borough of Manhattan, New York City, stretching from 86th to 96th Streets. It covers 106 acres and holds over 10⁹ US Gal. of water.",
       icon: {
         size: new window.google.maps.Size(40, 30),
         scaledSize: new window.google.maps.Size(40, 30),
@@ -70,14 +70,14 @@ const PopularRunCentralPark = () => {
     },
     }); //Jacqueline Kennedy Onassis Reservoir, New York, NY 10128
 
-    const image5 = "https://www.fitnyc.edu/images/cer/master-brand-button.jpg";
+    const image5 = "https://www.moma.org/assets/visit/entrance-image--museum-crop-7516b01003659172f2d9dbc7a6c2e9d9.jpg";
     const beachMarker5 = new window.google.maps.Marker({
       position: { lat: 40.781274, lng: -73.962803 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "Museum of Modern Art: Founded in 1929, The Museum of Modern Art (MoMA) in midtown Manhattan was the first museum devoted to the modern era.",
       icon: {
-        size: new window.google.maps.Size(40, 30),
-        scaledSize: new window.google.maps.Size(40, 30),
+        size: new window.google.maps.Size(40, 35),
+        scaledSize: new window.google.maps.Size(40, 35),
         url: image5
     },
     }); //The Metropolitan Museum of Art, 1000 5th Ave, New York, NY 10028
@@ -86,7 +86,7 @@ const PopularRunCentralPark = () => {
     const beachMarker6 = new window.google.maps.Marker({
       position: { lat: 40.783029, lng: -73.974219 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "American Museum of Natural History: The American Museum of Natural History is a natural history museum on the Upper West Side of Manhattan in New York City.",
       icon: {
         size: new window.google.maps.Size(40, 30),
         scaledSize: new window.google.maps.Size(40, 30),
@@ -98,7 +98,7 @@ const PopularRunCentralPark = () => {
     const beachMarker7 = new window.google.maps.Marker({
       position: { lat: 40.775945, lng: -73.970957 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "Bethesda Fountain: Bethesda Terrace and Fountain are two architectural features overlooking the southern shore of the Lake in New York City's Central Park. The fountain, with its Angel of the Waters statue, is located in the center of the terrace.",
       icon: {
         size: new window.google.maps.Size(40, 30),
         scaledSize: new window.google.maps.Size(40, 30),
@@ -110,7 +110,7 @@ const PopularRunCentralPark = () => {
     const beachMarker8 = new window.google.maps.Marker({
       position: { lat: 40.771785, lng: -73.974819 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "Sheep Meadow: Sheep Meadow is a 15-acre meadow near the southwestern section of Central Park, between West 66th and 69th Streets in Manhattan, New York City. It is adjacent to Central Park Mall to the east, The Ramble and Lake to the north, West Drive to the west, and Heckscher Playground and Ballfields to the south.",
       icon: {
         size: new window.google.maps.Size(40, 30),
         scaledSize: new window.google.maps.Size(40, 30),
@@ -122,7 +122,7 @@ const PopularRunCentralPark = () => {
     const beachMarker9 = new window.google.maps.Marker({
       position: { lat: 40.76873, lng: -73.974476 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "Wollman Rink: Wollman Rink is a public ice rink in the southern part of Central Park, Manhattan, New York City. It is named after the Wollman family who donated the funds for its original construction. The rink is open for ice skating from late October to early April.",
       icon: {
         size: new window.google.maps.Size(40, 30),
         scaledSize: new window.google.maps.Size(40, 30),
@@ -130,14 +130,14 @@ const PopularRunCentralPark = () => {
     },
     }); //Wollman Rink, Central Park S, New York, NY 10019
 
-    const image10 = "https://www.fitnyc.edu/images/cer/master-brand-button.jpg";
+    const image10 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQojINAyZbTCMaTgTNDBKyvGMPo7SjL07jBHjhdkc2bS9el1Xad7bNPxpiKNzILzrIHwSQ&usqp=CAU";
     const beachMarker10 = new window.google.maps.Marker({
       position: { lat: 40.769055, lng: -73.981686 },
       map,
-      title: "Museum at FIT: Rotating exhibits & permanent collection of garments & accessories; free admission.",
+      title: "Columbus Circle: Columbus Circle is a traffic circle and heavily trafficked intersection in the New York City borough of Manhattan, located at the intersection of Eighth Avenue, Broadway, Central Park South, and Central Park West, at the southwest corner of Central Park.",
       icon: {
-        size: new window.google.maps.Size(40, 30),
-        scaledSize: new window.google.maps.Size(40, 30),
+        size: new window.google.maps.Size(40, 35),
+        scaledSize: new window.google.maps.Size(40, 35),
         url: image10
     },
     }); //Columbus Circle, 848 Columbus Cir, New York, NY 10019
