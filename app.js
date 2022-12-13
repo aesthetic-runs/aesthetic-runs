9f1ad38e2a48ae0735c2d8c1f9e77cc4ee4ce8b4
Matthew Apuya <m.apuya19@gmail.com>
2022-12-12 23:19:20 -0500

    clean back-end app.js

--- app.js
+++ app.js
@@ -127,14 +127,14 @@ app.post("/login", (request, response) => {
 // bcrypt.hash(request.body.password, 10).then().catch();
 
 // free endpoint
-app.get("/free-endpoint", (request, response) => {
-  response.json({ message: "You are free to access me anytime." });
-});
+// app.get("/free-endpoint", (request, response) => {
+//   response.json({ message: "You are free to access me anytime." });
+// });
 
 // authentication endpoint
-app.get("/auth-endpoint", auth, (request, response) => {
-  response.json({ message: "You are authorized to access me." });
-});
+// app.get("/auth-endpoint", auth, (request, response) => {
+//   response.json({ message: "You are authorized to access me." });
+// });
 
 // module.exports = app;
 app.listen(8000, () => console.log("Server is up!")); // Local
